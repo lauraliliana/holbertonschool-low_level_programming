cc54cf2cee3baf3c0c3770d4b1de66180c613dcc
lauraliliana <lali@LAPTOP-MTV6MSS0.localdomain>
2020-09-17 13:13:16 -0500

    C001fix1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,17 @@ int n, lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
-printf("Last digit of %d is %d ", n, lastDigit);
 if (lastDigit > 5)
 {
-printf("and is greater than 5");
+printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
 }
 else if (lastDigit == 0)
 {
-printf("and is 0");
+printf("Last digit of %d is %d and is 0", n, lastDigit);
 }
 else
 {
-printf("and is less than 6 and not 0");
+printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
 }
 return (0);
 }
