8f1f79bcd011793cb1f8f01b564af28eefc43127
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 12:33:24 -0500

    c0x02fixed1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
-
+/**
+ *print_alphabet - iterates printing each char of alphabet 
+ */
 void print_alphabet(void)
 {
 for (int i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
+ _putchar(10); 
 }
