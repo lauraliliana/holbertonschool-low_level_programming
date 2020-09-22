8748e8540c1f9bb89660a2ea8b0e46ff23ca6419
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 13:42:50 -0500

    c0x022

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,18 @@
 /**
  * print_alphabet_x10 - Operates
  *
- * Return: On success 1.
+ * Return: none.
  *
  */
 void print_alphabet_x10(void)
 {
-  int i, j;
-  for (j = 0; j <= 9; j++){
-  for (i = 'a'; i <= 'z'; i++)
-    {
-      _putchar(i);
-    }
-  _putchar(10);
+int i, j;
+for (j = 0; j <= 9; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar(10);
 }
 }
