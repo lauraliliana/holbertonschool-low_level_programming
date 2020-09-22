65f14e38792ec2f00ac33005645d0241bc014598
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 16:28:17 -0500

    c0x027

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the las digit
+ * @r: character to operate
+ * Return: last digit
+ *
+ */
+int print_last_digit(int r)
+{
+int ld;
+ld = r % 10;
+return (ld);
+}
