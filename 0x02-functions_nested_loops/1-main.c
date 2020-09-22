efb4afc722683c62c2a1a3baa6785f9ecf82ae23
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 13:04:46 -0500

    Corrections exe 1

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,11 +1,13 @@
-#include "holberton.h"
-		/**
-		 * main - check the code for Holberton School students.
-		 *
-		 * Return: Always 0.
-		 */
-int main(void)
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-  print_alphabet();
-  return (0);
+  return (write(1, &c, 1));
 }
