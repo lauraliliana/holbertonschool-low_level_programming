23c2222e948393f713a49f7df1fb6cce2b824636
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 15:06:44 -0500

    c0x02fix4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 /**
  * _isalpha - Operates
- * @c
+ * @c: operative variable
  * Return: 1 if c is a letter, 0 otherwise.
  *
  */
 int _isalpha(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    {
-      return (1);
-    }
-  else if (c >= 'A' && c <= 'Z')
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }  
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
