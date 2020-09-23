b95b80702721801cfd814cc12c65a30d0a879430
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 23:59:20 -0500

    c02365

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
  */
 void print_times_table(int n)
 {
-int i, j, r;
+int i, j, r, o, p;
 if (n > 0 && n <= 15)
 {
 for (i = 0; i < n; i++)
@@ -32,9 +32,17 @@ if (r < 10)
 {
 _putchar(r + '0');
 }
-else
+else if (r < 100)
 {
 _putchar(r / 10 + '0');
+_putchar(r % 10 + '0');
+}
+else
+{
+_putchar(r / 100 + '0');
+o = r / 100;
+p = r - o;
+_putchar(p / 10 + '0');
 _putchar(r % 10 + '0');
 }
 }
