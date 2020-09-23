92dd93b579ef1678e74b0dbbc54392766cc9a1b5
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-23 18:27:55 -0500

    c0x02a2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
  */
 void print_times_table(int n)
 {
-int i, j, r, o, p;
+int i, j, r;
 if (n > 0 && n <= 15)
 {
 for (i = 0; i < n; i++)
@@ -40,9 +40,7 @@ _putchar(r % 10 + '0');
 else
 {
 _putchar(r / 100 + '0');
-o = r / 100;
-p = r - o;
-_putchar(p / 10 + '0');
+_putchar((r - 100) / 10 + '0');
 _putchar(r % 10 + '0');
 }
 }
