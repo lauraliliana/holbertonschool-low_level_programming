dc7fd2b4d7c3548e4461c2316980004b5499d54c
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 21:09:43 -0500

    c0x0211try2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,19 @@
 void print_to_98(int n)
 {
 int i;
+if (n < 98)
+{
 for (i = n; i < 98; i++)
 {
 printf("%d, ", i);
 }
-printf("98");
+}
+else if (n > 98)
+{
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+}
+}
+printf("98\n");
 }
