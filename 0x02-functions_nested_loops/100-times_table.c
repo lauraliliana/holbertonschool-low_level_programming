275ba199d6fbe60710080ebcc0100c5d17614950
root <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-22 23:51:00 -0500

    c0x02a1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * print_times_table - prints the times table
+ * @n: the limit
+ * Return: void
+ *
+ */
+void print_times_table(int n)
+{
+int i, j, r;
+if (n > 0 && n <= 15)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+r = i * j;
+if (j > 0 && j < n)
+{
+_putchar(44);
+_putchar(32);
+if (r < 100)
+{
+_putchar(32);
+if (r < 10)
+{
+_putchar(32);
+}
+}
+}
+if (r < 10)
+{
+_putchar(r + '0');
+}
+else
+{
+_putchar(r / 10 + '0');
+_putchar(r % 10 + '0');
+}
+}
+_putchar(10);
+}
+}
+}
