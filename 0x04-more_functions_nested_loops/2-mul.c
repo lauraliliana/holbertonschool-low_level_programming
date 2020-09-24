0a5f608b010dc3bf2a473417a0d956a8e740f7e1
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-24 14:02:02 -0500

    c0x04until6

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+int m;
+m = a * b;
+return (m);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints number from 0 to
+ * Return: void
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+_putchar(n+'0');
+}
+_putchar(10);
+}
