19b7128f4cf5ea66b70afd4b6e1b4610af064040
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-23 19:00:45 -0500

    c0x02advanced100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@ void print_times_table(int n)
 int i, j, r;
 if (n > 0 && n <= 15)
 {
-for (i = 0; i < n; i++)
+for (i = 0; i <= n; i++)
 {
-for (j = 0; j < n; j++)
+for (j = 0; j <= n; j++)
 {
 r = i * j;
-if (j > 0 && j < n)
+if (j > 0 && j <= n)
 {
 _putchar(44);
 _putchar(32);
