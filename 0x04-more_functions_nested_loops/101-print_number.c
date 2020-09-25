a7b20268348f2045dffe3f4bd84bac1f23c2d045
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-24 22:53:41 -0500

    c0x04advfix4

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@ void print_number(int n)
 if (n < 0)
 {
 _putchar (45);
-n = n * -1;
+n = -n;
 }
 if (n >= 1000)
 {
@@ -23,5 +23,5 @@ if (n >= 10)
 {
 _putchar (n / 10 % 10 + '0');
 }
-_putchar (n % 10 + '0');
+_putchar (n %10  + '0');
 }
