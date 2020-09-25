3ca8d84f5dd4d760261d0d4f6c08b8162d6c2d11
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-24 22:27:42 -0500

    c0x0412last

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,7 +6,6 @@
  */
 void print_number(int n)
 {
-int a, b, c;
 if (n < 0)
 {
 _putchar (45);
@@ -14,18 +13,15 @@ n = n * -1;
 }
 if (n >= 1000)
 {
-a = n / 1000;
-_putchar (a % 10 + '0');
+_putchar (n / 1000 % 10 + '0');
 }
 if (n >= 100)
 {
-b = n / 100;
-_putchar (b % 10 + '0');
+_putchar (n / 100 % 10 + '0');
 }
 if (n >= 10)
 {
-c = n / 10;
-_putchar (c % 10 + '0');
+_putchar (n / 10 % 10 + '0');
 }
 _putchar (n % 10 + '0');
 }
