8409223c5d3157afe11187af25232960135b7b48
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-09-29 21:18:58 -0500

    c0x054

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_rev - prints a string in reverse.
+ * @s: string a imprimir.
+ * Return: none.
  */
 void print_rev(char *s)
 {
-
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+for (j = i; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar(10);
 }
