9cb66ea17c042c8c81c71f5862941babecbb7024
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-10-01 23:51:01 -0500

    c0x068betty

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -10,10 +10,10 @@
  */
 char *rot13(char *s)
 {
-  int i, j;
-  char rot13a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char rot13b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  j = 0;
+int i, j;
+char rot13a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot13b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+j = 0;
 while (s[j] != 0)
 {
 for (i = 0; i < 52; i++)
