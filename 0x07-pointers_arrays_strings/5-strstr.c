e6f7d98e0a4f4184948b45bca67a064dc83b6556
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-10-05 23:47:26 -0500

    c0x07.6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
