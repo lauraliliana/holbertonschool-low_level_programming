ea7ab600d66e87318347b72d195e180a68836fdc
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-10-15 19:00:45 -0500

    0x0C. C 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include <stdlib.h
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: amount of memory to allocate.
+ * Return: None.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b);
+}
