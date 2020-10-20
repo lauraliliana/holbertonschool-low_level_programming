944fb591b723b96c56f20bf1121191fb44ca72db
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-10-19 23:22:10 -0500

    0x0E. C - 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: name, age and owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
