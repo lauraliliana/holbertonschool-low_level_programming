a9cde3ce3e803e157f358870cb5d849bdbb3fb76
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-10-21 23:09:38 -0500

    0x0F. C - Function pointers exercise 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+/**
+ * get_op_func - selects the correct function
+ * @s: input symbol
+ * Return: pointer to operation function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s))
+		{
+			i++;
+		}
+		else
+		{
+			return (ops[i].f);
+		}
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: input integer a
+ * @b: input integer b
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: input integer a
+ * @b: input integer b
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: input integer a
+ * @b: input integer b
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	printf("Error\n");
+	exit(100);
+}
+
+/**
+ * op_mod - modulo of a / b
+ * @a: input integer a
+ * @b: input integer b
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	printf("Error\n");
+	exit(100);
+}
