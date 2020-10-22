9097bd976dbd7abefab0954b1dcaa94c1db31ca8
lauraliliana <root@LAPTOP-MTV6MSS0.localdomain>
2020-10-21 21:26:00 -0500

    0x0F. C - Function pointers exercise 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: pointer to function
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: pointer to array
+ * @size: size of array
+ * @action: function
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	
+}
